2131e0e596c374d2f3822ff43e11ca8e4d91502d
melian <meliangalovic@gmail.com>
2019-02-27 13:14:09 +0100

    start page started

--- src/app/components/scui-nav/scui-nav.component.ts
+++ src/app/components/scui-nav/scui-nav.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ScUiMenuItem } from '../../interfaces';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {ScUiMenuItem} from '../../interfaces';
 
 @Component({
   selector: 'scui-nav',
@@ -7,17 +7,17 @@ import { ScUiMenuItem } from '../../interfaces';
     <aside class="scui-nav">
       <header class="nav-header" *ngIf="logo">
         <a [routerLink]="[logo.route]" [ngClass]="logo.cssClass">
-          <img [src]="logo.iconUrl" [alt]="logo.name" />
+          <img [src]="logo.iconUrl" [alt]="logo.name"/>
         </a>
       </header>
       <nav class="nav-items">
         <a class="nav-item" *ngFor="let menuItem of menuItems" [routerLink]="[menuItem.route]">
-          <img [src]="menuItem.iconUrl" [alt]="menuItem.name" />
+          <img [src]="menuItem.iconUrl" [alt]="menuItem.name"/>
         </a>
       </nav>
     </aside>
   `,
-  styleUrls: ['./scui-nav.component.scss']
+  encapsulation: ViewEncapsulation.None
 })
 export class ScuiNavComponent implements OnInit {
   @Input() menuItems: ScUiMenuItem[];

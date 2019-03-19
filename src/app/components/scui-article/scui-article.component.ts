9f0f106ec08beb36822b6340ba6900fbaa576356
melian <meliangalovic@gmail.com>
2019-03-19 17:12:18 +0100

    article states

--- src/app/components/scui-article/scui-article.component.ts
+++ src/app/components/scui-article/scui-article.component.ts
@@ -5,23 +5,55 @@ import { ScUiArticle } from '../../interfaces';
   selector: 'scui-article',
   template: `
     <article class="scui-article">
-      <figure class="scui-article-img">
-        <img [src]="article.photoUrl" [alt]="article.title"/>
-      </figure>
-      <aside class="scui-article-content">
-        <header class="scui-article-header">
-          <time>{{article.date}}</time>
-          <a href="#"><span class="icon icon-globe"></span></a>
-        </header>
-        <div class="scui-article-desc">
-          <h3>{{article.title}}</h3>
-          <p><a [href]="article.url" target="_blank">{{article.description}}</a></p>
+      <article class="delete" *ngIf="article.mode === 'delete'">
+        <div class="bar"></div>
+        <div class="header">
+          <div>
+            <span class="icon icon-c-warning"></span>
+            <span>REMOVE ARTICLE</span>
+          </div>
         </div>
-        <footer class="scui-article-footer">
-          <button class="icon-btn" (click)="importArticle(article)"><span class="icon icon-pen-2"></span></button>
-          <button class="icon-btn"><span class="icon icon-e-delete"></span></button>
-        </footer>
-      </aside>
+        <div class="content">
+
+        </div>
+      </article>
+      <article class="edit" *ngIf="article.mode === 'edit'">
+        <figure [ngStyle]="{'background-image': 'url(' + article.photoUrl + ')'}" class="scui-article-img">
+          <div class="img"></div>
+        </figure>
+        <aside class="scui-article-content">
+          <header class="scui-article-header">
+            <time>{{article.date}}</time>
+            <a href="#"><span class="icon icon-globe"></span></a>
+          </header>
+          <div class="scui-article-desc">
+            <h3>{{article.title}}</h3>
+            <p><a [href]="article.url" target="_blank">{{article.description}}</a></p>
+          </div>
+          <footer class="scui-article-footer">
+            <button class="icon-btn" (click)="importArticle(article)"><span class="icon icon-pen-2"></span></button>
+            <button class="icon-btn"><span class="icon icon-check-single"></span></button>
+          </footer>
+        </aside>
+      </article>
+      <article class="default" *ngIf="article.mode === 'default'">
+        <figure [ngStyle]="{'background-image': 'url(' + article.photoUrl + ')'}" class="scui-article-img">
+          <div class="img"></div>
+        </figure>
+        <aside class="scui-article-content">
+          <header class="scui-article-header">
+            <time>{{article.date}}</time>
+            <a href="#"><span class="icon icon-globe"></span></a>
+          </header>
+          <div class="scui-article-desc">
+            <h3>{{article.title}}</h3>
+            <p><a [href]="article.url" target="_blank">{{article.description}}</a></p>
+          </div>
+          <footer class="scui-article-footer">
+            <span class="icon icon-e-add"></span>
+          </footer>
+        </aside>
+      </article>
     </article>
   `,
   encapsulation: ViewEncapsulation.None

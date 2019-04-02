de0a73efba05c862d998d7d1cb18405ca2e52913
avramz <avramovic.u@gmail.com>
2019-04-02 16:58:13 +0200

    - use routerLink for card component

--- src/app/components/scui-card/scui-card.module.ts
+++ src/app/components/scui-card/scui-card.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScUiCardComponent } from './scui-card.component';
 import { BgHoverModule } from '../../directives/bg-hover.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ScUiCardComponent],
   exports: [ScUiCardComponent],
   imports: [
     CommonModule,
-    BgHoverModule
+    BgHoverModule,
+    RouterModule
   ]
 })
 export class ScUiCardModule {

1bb67cac4421f35065b4cdb33d840b2d9f97a366
melian <meliangalovic@gmail.com>
2019-04-02 10:05:36 +0200

    CCE-1260 tabs file deleted

--- src/stories/13.tabs.stories.ts
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from '@storybook/angular';
-import { ScuiTabsComponent } from '../app/components/scui-tabs/scui-tabs.component';
-
-storiesOf('Tabs', module)
-  .add('test', () => ({
-    component: ScuiTabsComponent
-  }));

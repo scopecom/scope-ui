d502b7bccb86468da7685ffe65dd359f3cd5a818
avramz <avramovic.u@gmail.com>
2019-04-03 17:51:55 +0200

    - tabs refactor

--- src/app/components/scui-tabs/scui-tabs.component.ts
+++ src/app/components/scui-tabs/scui-tabs.component.ts
@@ -1,26 +1,34 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, OnInit, QueryList, ViewChildren, ViewEncapsulation } from '@angular/core';
 import { Tabs } from '../../constants/tabs';
 
 @Component({
   selector: 'scui-tabs',
   template: `
     <section class="scui-tabs">
-      <div class="tabs">
+      <div class="tabs" #tabContainer>
         <div *ngFor="let tab of tabs, let i = index"
              [ngClass]="{'active-tab': i === activeTab}"
-             (click)="setActiveTab(i)"
+             (click)="setActiveTab(i, tabItem, tabContainer)"
+             #tabItem
              class="tab">{{ tab.title }}
         </div>
         <div class="border-wrap">
-          <div [ngStyle]="{left: activeTab * 75 + 'px' }" class="border-bottom"></div>
+          <div [ngStyle]="{left: (offsetLeft || 0) + 'px', width: (offsetWidth) + 'px' }"
+               class="border-bottom">
+          </div>
         </div>
       </div>
     </section>
   `,
   encapsulation: ViewEncapsulation.None
 })
-export class ScuiTabsComponent implements OnInit {
+export class ScuiTabsComponent implements OnInit, AfterViewInit {
   activeTab: number;
+  offsetLeft: number;
+  offsetWidth: number;
+
+  @ViewChildren('tabItem') tabItems: QueryList<HTMLDivElement>;
+
   tabs = Tabs;
 
   constructor() {
@@ -30,8 +38,16 @@ export class ScuiTabsComponent implements OnInit {
     this.setActiveTab(0);
   }
 
-  setActiveTab(key) {
-    this.activeTab = key;
+  ngAfterViewInit() {
+    this.setActiveTab(0, this.tabItems.first);
   }
 
+  setActiveTab(key, tab?: HTMLDivElement, tabContainer?: HTMLDivElement) {
+    this.activeTab = key;
+
+    if (tab) {
+      this.offsetLeft = tabContainer ? tab.offsetLeft - tabContainer.offsetLeft : 0;
+      this.offsetWidth = tab.offsetWidth;
+    }
+  }
 }
--- src/app/constants/tabs.ts
+++ src/app/constants/tabs.ts
@@ -1,17 +1,11 @@
 export const Tabs = [
   {
-    title: 'ITEM 1',
+    title: 'FACEBOOK',
   },
   {
-    title: 'ITEM 2',
+    title: 'TWITTER',
   },
   {
-    title: 'ITEM 3',
-  },
-  {
-    title: 'ITEM 4',
-  },
-  {
-    title: 'ITEM 5',
+    title: 'LINKEDIN',
   }
 ];

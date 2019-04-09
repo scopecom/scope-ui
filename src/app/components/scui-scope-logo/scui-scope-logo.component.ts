e10431e71e4e2a5127518a9cafca64f3f8c8f34b
melian <meliangalovic@gmail.com>
2019-04-09 15:43:35 +0200

    placeholder component

---
+++ src/app/components/scui-scope-logo/scui-scope-logo.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'scui-scope-logo',
+  template: `
+    <div>Placeholder</div>
+  `,
+})
+export class ScUiScopeLogoComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+
+}
--- src/stories/06.data-display.stories.ts
+++ src/stories/06.data-display.stories.ts
@@ -1,12 +1,14 @@
 import { moduleMetadata, storiesOf } from '@storybook/angular';
 import { CommonModule } from '@angular/common';
-import { ScUiArticleComponent } from '../app/components/scui-article/scui-article.component';
+import { ScUiScopeLogoComponent } from '../app/components/scui-scope-logo/scui-scope-logo.component';
 
 storiesOf('Data Display', module)
   .addDecorator(
     moduleMetadata({
       imports: [CommonModule],
-      declarations: [ScUiArticleComponent]
+      declarations: [ScUiScopeLogoComponent]
     })
-  );
+  ).add('Placeholder Component', () => ({
+  template: `<scui-scope-logo></scui-scope-logo>`,
+}));
 

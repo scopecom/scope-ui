886f4850c13f984ea23b7c8e332a8d983368ae4f
melian <meliangalovic@gmail.com>
2019-04-15 10:54:49 +0200

    0.0.401

--- src/app/public_api.ts
+++ src/app/public_api.ts
@@ -1,13 +1,15 @@
 /*
  * Public API Surface of scope-ui
  */
-export * from './components/scui-pills/scui-pills.module';
+// modules
 export * from './components/scui-article/scui-article.module';
 export * from './components/scui-card/scui-card.module';
 export * from './components/scui-drawer/scui-drawer.module';
+export * from './components/scui-dropdown/scui-dropdown.module';
 export * from './components/scui-nav/scui-nav.module';
+export * from './components/scui-pills/scui-pills.module';
 export * from './components/scui-tabs/scui-tabs.module';
-
+// directives
 export * from './directives/bg-hover.module';
-
+// interfaces
 export * from './interfaces';

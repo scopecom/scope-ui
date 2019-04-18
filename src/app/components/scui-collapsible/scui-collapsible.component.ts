b7eede07dbc8ce4137175cd9fd025757eda6c10a
melian <meliangalovic@gmail.com>
2019-04-18 12:41:07 +0200

    collapsible review changes

--- src/app/components/scui-collapsible/scui-collapsible.component.ts
+++ src/app/components/scui-collapsible/scui-collapsible.component.ts
@@ -7,13 +7,19 @@ import { Component, Input, OnInit } from '@angular/core';
       <div class="collapsible-inner">
         <div class="collapsible-controls">
           <div (click)="isExpanded = !isExpanded">
-            <span *ngIf="!isExpanded" class="icon icon-right-arrow"></span>
-            <span *ngIf="isExpanded" class="icon icon-down-arrow"></span>
-            <span class="label">MODULES</span>
+            <span class="group">
+              <span *ngIf="!isExpanded" class="icon icon-right-arrow"></span>
+              <span *ngIf="isExpanded" class="icon icon-down-arrow"></span>
+              <span class="label">MODULES</span>
+            </span>
           </div>
           <div>
-            <span class="icon icon-e-add"></span>
-            <span class="icon icon-settings-gear"></span>
+            <span class="icon-wrap">
+              <span class="icon icon-e-add"></span>
+            </span>
+            <span class="icon-wrap">
+              <span class="icon icon-settings-gear"></span>
+            </span>
           </div>
         </div>
         <div *ngIf="isExpanded" class="collapsible-content">{{ content }}</div>

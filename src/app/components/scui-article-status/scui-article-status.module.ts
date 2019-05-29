cde8b90d62c6fb9adab08cd53f2113fe420879ad
melian <meliangalovic@gmail.com>
2019-05-29 14:27:00 +0200

    article element

---
+++ src/app/components/scui-article-status/scui-article-status.module.ts
@@ -0,0 +1,133 @@
+import {CommonModule} from '@angular/common';
+import {MatMenuModule} from '@angular/material/menu';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
+import {
+  Component,
+  Input,
+  OnInit,
+  NgModule
+} from '@angular/core';
+
+@Component({
+  selector: 'scui-article-status',
+  template: `
+    <article class="scui-article-status">
+      <div class="icons-wrap">
+        <span class="item" *ngFor="let itm of outputChannels">
+          <span class="icon-wrap {{ itm.type }}"
+                [ngClass]="{scheduled:itm.status === statusScheduled, published:itm.status === statusPublished,
+                 defaultState: itm.status !== statusPublished && itm.status !== statusScheduled}">
+            <span class="icon {{ channelsConfig[itm.type].icon }}"></span>
+                      <span *ngIf="itm.status === statusPublished || itm.status === statusScheduled" class="dot"></span>
+          </span>
+        </span>
+      </div>
+      <div class="info-wrap">
+        <div class="info-top">
+          <div class="info-author">{{ author }}</div>
+          <div class="info-title">{{ title }}</div>
+        </div>
+        <div class="info-bottom">
+          <span class="counter">
+            <span>{{ outputChannelsTotal }}</span>
+          </span>
+          <span>
+            <span [matMenuTriggerFor]="menu" class="icon icon-menu-ham-horz menu-trigger"></span>
+                  <mat-menu class="output-actions" #menu="matMenu">
+                    <div class="output-action disabled" mat-menu-item>
+                      <span class="icon icon-pen-2"></span>
+                      <span class="output-action-label"> Edit</span>
+                    </div>
+                    <div class="output-action" mat-menu-item>
+                      <span class="icon icon-preview"></span>
+                      <span class="output-action-label"> View</span>
+                    </div>
+                    <div class="output-action" mat-menu-item>
+                      <span class="icon icon-box"></span>
+                      <span class="output-action-label"> Archive</span>
+                    </div>
+                    <div class="output-action" mat-menu-item>
+                      <span class="icon icon-send"></span>
+                      <span class="output-action-label"> Publish</span>
+                    </div>
+                    <div class="output-action output-action-last" mat-menu-item>
+                      <span class="icon icon-bin"></span>
+                      <span class="output-action-label"> Delete</span>
+                    </div>
+                  </mat-menu>
+          </span>
+        </div>
+      </div>
+    </article>
+  `,
+})
+export class ScUiArticleStatusComponent implements OnInit {
+
+  @Input() author: string;
+  @Input() title: string;
+  @Input() outputChannels: any;
+  outputChannelsTotal = 0;
+  readonly statusPublished = 'published';
+  readonly statusScheduled = 'scheduled';
+
+  channelsConfig = {
+    TWITTER: {
+      icon: 'icon-twitter'
+    },
+    WORDPRESS: {
+      icon: 'icon-wordpress'
+    },
+    MAILCHIMP: {
+      icon: 'icon-mc-freddie-dark'
+    },
+    FACEBOOK_PAGE: {
+      icon: 'icon-facebook-f'
+    },
+    LINKEDIN_PAGE: {
+      icon: 'icon-linkedin-in'
+    },
+    // LINKEDIN_PROFILE: {
+    //   icon: ''
+    // },
+    GOOGLE_DOC: {
+      icon: 'icon-google-drive'
+    },
+    SALES_FORCE: {
+      icon: 'icon-typo3'
+    },
+    SCOPE_TO_EMAIL: {
+      icon: 'icon-mail-block'
+    },
+    HUBSPOT: {
+      icon: 'icon-HubSpot'
+    },
+  };
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    if (this.outputChannels) {
+      const activeChannels = this.outputChannels.filter(val => val.status === this.statusPublished || val.status === this.statusScheduled);
+      if (activeChannels && activeChannels.length) {
+        this.outputChannelsTotal = activeChannels.length;
+      }
+    }
+  }
+
+}
+
+@NgModule({
+  declarations: [ScUiArticleStatusComponent],
+  imports: [
+    CommonModule,
+    MatMenuModule,
+    NoopAnimationsModule
+  ],
+  exports: [ScUiArticleStatusComponent]
+})
+export class ScUiArticleStatusModule {
+}
+
+
--- src/stories/190.file-list.stories.ts
+++ src/stories/190.file-list.stories.ts
@@ -1,6 +1,54 @@
-import {storiesOf} from '@storybook/angular';
+import {moduleMetadata, storiesOf} from '@storybook/angular';
+import {ScUiArticleStatusModule} from '../app/components/scui-article-status/scui-article-status.module';
 
 storiesOf('File Lists', module)
-  .add('placeholder', () => ({
-    template: `<div>Placeholder</div>`
+  .addDecorator(moduleMetadata({
+    imports: [ScUiArticleStatusModule]
+  }))
+  .add('Article element', () => ({
+    template: `<div style="padding: 20px;">
+      <scui-article-status [author]="author" [title]="title" [outputChannels]="outputChannels" ></scui-article-status>
+      </div>`,
+    props: {
+      author: 'Author',
+      title: 'Title',
+      outputChannels: [
+        {
+          type: 'SCOPE_TO_EMAIL',
+          status: ''
+        },
+        {
+          type: 'FACEBOOK_PAGE',
+          status: ''
+        },
+        {
+          type: 'LINKEDIN_PAGE',
+          status: 'published'
+        },
+        {
+          type: 'TWITTER',
+          status: 'scheduled'
+        },
+        {
+          type: 'WORDPRESS',
+          status: 'published'
+        },
+        {
+          type: 'GOOGLE_DOC',
+          status: 'published'
+        },
+        {
+          type: 'MAILCHIMP',
+          status: 'published'
+        },
+        {
+          type: 'SALES_FORCE',
+          status: 'scheduled'
+        },
+        {
+          type: 'HUBSPOT',
+          status: 'published'
+        }
+      ]
+    }
   }));

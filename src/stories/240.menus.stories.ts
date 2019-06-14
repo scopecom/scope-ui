b85b219302aba4c9db4fafb7080f4c87eef1376e
melian <meliangalovic@gmail.com>
2019-06-14 14:49:31 +0200

    submenu logout

--- src/stories/240.menus.stories.ts
+++ src/stories/240.menus.stories.ts
@@ -1,23 +1,24 @@
-import { moduleMetadata, storiesOf } from '@storybook/angular';
-import { action } from '@storybook/addon-actions';
-import { CommonModule } from '@angular/common';
-import { ScUiCardComponent } from '../app/components/scui-card/scui-card.component';
-import { ButtonListStub } from '../app/constants/button.list';
-import { BgHoverDirective } from '../app/directives/bg-hover.module';
-import { ScUiDrawerComponent, ScUiDrawerNavComponent } from '../app/components/scui-drawer/scui-drawer.component';
-import { ScUiNavComponent } from '../app/components/scui-nav/scui-nav.component';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppMenu, AppMenuFooter, AppMenuLogo } from '../app/constants/app.menu';
-import { SubmenuStub1, SubmenuStub2 } from '../app/constants/submenu';
-import { ScUiTabsComponent } from '../app/components/scui-tabs/scui-tabs.component';
-import { DragulaModule } from 'ng2-dragula';
-import { Tabs } from '../app/constants/tabs';
+import {moduleMetadata, storiesOf} from '@storybook/angular';
+import {action} from '@storybook/addon-actions';
+import {CommonModule} from '@angular/common';
+import {ScUiCardComponent} from '../app/components/scui-card/scui-card.component';
+import {ButtonListStub} from '../app/constants/button.list';
+import {BgHoverDirective} from '../app/directives/bg-hover.module';
+import {ScUiDrawerComponent, ScUiDrawerNavComponent} from '../app/components/scui-drawer/scui-drawer.component';
+import {ScUiNavComponent} from '../app/components/scui-nav/scui-nav.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppMenu, AppMenuFooter, AppMenuLogo} from '../app/constants/app.menu';
+import {SubmenuStub1, SubmenuStub2} from '../app/constants/submenu';
+import {ScUiTabsComponent} from '../app/components/scui-tabs/scui-tabs.component';
+import {DragulaModule} from 'ng2-dragula';
+import {MatMenuModule} from '@angular/material/menu';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 
 storiesOf('Menus', module)
   .addDecorator(
     moduleMetadata({
-      imports: [CommonModule, RouterTestingModule, DragulaModule.forRoot()],
+      imports: [CommonModule, RouterTestingModule, DragulaModule.forRoot(), MatMenuModule, NoopAnimationsModule],
       declarations: [ScUiCardComponent, ScUiTabsComponent, BgHoverDirective, ScUiDrawerComponent, ScUiNavComponent, ScUiDrawerNavComponent]
     })
   )
@@ -78,6 +79,23 @@ storiesOf('Menus', module)
       submenu2: SubmenuStub2,
       drawerOpen: true
     },
+  }))
+  .add('Logout submenu', () => ({
+    template: `
+          <div style="margin:20px;">
+                  <mat-menu #appMenu="matMenu">
+                    <div class="logout-item username" mat-menu-item>
+                    <span>Peter Hogenkamp</span>
+                    </div>
+                    <div class="logout-item logout" mat-menu-item>
+                    <span>Logout</span>
+                    </div>
+                  </mat-menu>
+                  <button style="padding:5px;" [matMenuTriggerFor]="appMenu">
+                    Toggle Logout submenu
+                  </button>
+          </div>
+    `
   }))
   .add('Menu Default', () => ({
     template: `

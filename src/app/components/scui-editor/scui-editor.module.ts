64a444065b7680600e8168e3cad0ca6f824d75d9
avramz <avramovic.u@gmail.com>
2019-07-18 10:01:58 +0200

    - 0.0.471  - add image crop directive

--- src/app/components/scui-editor/scui-editor.module.ts
+++ src/app/components/scui-editor/scui-editor.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScUiEditorArticleComponent } from './scui-editor-article/scui-editor-article.component';
 import { ScUiEditorElementComponent } from './scui-editor-element/scui-editor-element.component';
+import { ImageCropModule } from '../../directives/image-crop.module';
 
 @NgModule({
   declarations: [ScUiEditorArticleComponent, ScUiEditorElementComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ImageCropModule
   ],
   exports: [ScUiEditorArticleComponent, ScUiEditorElementComponent]
 })

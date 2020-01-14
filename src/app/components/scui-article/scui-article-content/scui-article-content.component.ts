1a2374d6ca325d760d4763108a0d2e8967099047
Predrag Jovanovic <j.predrag@gmail.com>
2020-01-14 18:47:16 +0100

    SUI-177 add comment in editor Article tab 0.0.545 update version

--- src/app/components/scui-article/scui-article-content/scui-article-content.component.ts
+++ src/app/components/scui-article/scui-article-content/scui-article-content.component.ts
@@ -8,11 +8,6 @@ import {
   selector: 'scui-article-content',
   template: `
     <div class="scui-article-content {{ mode }}">
-      <header class="scui-article-header">
-        <time *ngIf="date && date !== ''">{{ date | date:'d.M.yyyy' }}</time>
-        <span *ngIf="date && date !== ''"> - </span>
-        <span>{{ source }}</span>
-      </header>
       <div class="scui-article-desc">
         <h3><a [href]="url" target="_blank">{{headline}}</a></h3>
         <p class="scui-article-desc__comment">{{ commentFinal }}</p>

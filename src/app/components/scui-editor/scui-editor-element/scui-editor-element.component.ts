5eb4baa366aeaebd482bce93cdaadb99209bd5d3
avramovic.u <avramovic.u@gmail.com>
2020-02-27 10:10:29 +0100

    Merge branch 'master' of /Users/avramovic.u/IdeaProjects/scope-ui with conflicts.

--- src/app/components/scui-editor/scui-editor-element/scui-editor-element.component.ts
+++ src/app/components/scui-editor/scui-editor-element/scui-editor-element.component.ts
@@ -1,10 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'scui-editor-element',
   template: `
     <div class="scui-editor-element">
       <div class="handle"></div>
+      <div class="insert-el i-top" (click)="$event.preventDefault(); insertEvt.emit('top')" role="button">
+        <span class="icon-frame-s-caret-up"></span>
+      </div>
+      <div class="insert-el i-bot" (click)="$event.preventDefault(); insertEvt.emit('bottom')" role="button">
+        <span class="icon-frame-s-caret-down"></span>
+      </div>
       <div class="scui-editor-element-icon">
         <span class="icon" [ngClass]="elementMeta?.iconClass"></span>
       </div>
@@ -17,6 +23,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ScUiEditorElementComponent implements OnInit {
   @Input() element: any;
   @Input() elementMeta: { label: string; iconClass: string };
+
+  @Output() insertEvt = new EventEmitter<'top' | 'bottom'>();
 
   constructor() {
   }
